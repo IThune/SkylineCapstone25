4e638cf403dcc3622383887e3c6eec49f50229e0
mec <ian-thune@student.kirkwood.edu>
2025-05-03 22:23:50 -0500

    zoneminder script work

--- scripts/configurezoneminder.sh
+++ scripts/configurezoneminder.sh
@@ -7,4 +7,51 @@
 # $4 - Github API Token to download files
 # $5 - Azure Linux Agent Actions config file name
 
-cat "Deploy Success!" > /home/zmadmin/success.txt
+# Step 1 - Create a filesystem on and mount the 64GB footage disk
+DISK="/dev/disk/azure/scsi1/lun0"
+MOUNT_POINT="/mnt/footage"
+
+# install necessary utilities
+apt update && apt upgrade -y && apt install parted -y
+
+# Create a partition if not already present
+if ! lsblk "$DISK" | grep -q part; then
+  parted "$DISK" --script mklabel gpt mkpart primary ext4 0% 100%
+  mkfs.ext4 "${DISK}1"
+fi
+
+# Create mount point and mount the disk
+mkdir -p "$MOUNT_POINT"
+mount "${DISK}1" "$MOUNT_POINT"
+
+# Persist in /etc/fstab
+UUID=$(blkid -s UUID -o value "${DISK}1")
+echo "UUID=$UUID $MOUNT_POINT ext4 defaults,nofail 0 2" >> /etc/fstab
+
+# Step 2 - Install Zoneminder package and dependencies
+apt install apache2 mariadb-server zoneminder -y
+
+# Step 3 - Create the zoneminder database and database user
+mariadb <<EOF
+CREATE DATABASE zm;
+CREATE USER $1@localhost IDENTIFIED BY '$2';
+GRANT ALL ON zm.* TO $1@localhost;
+FLUSH PRIVILEGES;
+exit;
+EOF
+
+# Step 4 - Configure the ZM database
+mariadb -u $1 -p"$2" < /usr/share/zoneminder/db/zm_create.sql
+
+# Step 5 - Set permissions for zm.conf
+chgrp -c www-data /etc/zm/zm.conf
+
+# Step 6 - Set the Apache configuration
+a2enconf zoneminder # enable Zoneminder apache2 config
+a2enmod cgi # enable cgi module
+# reload, start and enable services
+systemctl reload apache2.service
+systemctl restart zoneminder.service
+systemctl status enable zoneminder.service
+
+# Step 7 - perform final configuration of zm.conf
